deca8bd66b9e33c24b812d5163012cf03acdf9d6
Arthur Bruno Moreira Barbosa <arthur.barbosa@ITBL000531D.itbam.org.br>
2021-01-19 13:04:00 -0400

    Created project backend

---
+++ abmoney-backend/abmoney/src/main/java/com/abcode/abmoney/AbmoneyApplication.java
@@ -0,0 +1,13 @@
+package com.abcode.abmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbmoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbmoneyApplication.class, args);
+	}
+
+}
---
+++ abmoney-backend/abmoney/src/test/java/com/abcode/abmoney/AbmoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abcode.abmoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbmoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
