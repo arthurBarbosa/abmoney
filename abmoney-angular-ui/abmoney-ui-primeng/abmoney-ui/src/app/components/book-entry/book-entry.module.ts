701e38b3df8501cd97b2f2df2ffecd7f3328a0e2
Arthur Bruno Moreira Barbosa <arthur.barbosa@ITBL000531D.itbam.org.br>
2021-02-01 10:59:47 -0400

    created module  grid-book-entry and person-grid

---
+++ abmoney-angular-ui/abmoney-ui-primeng/abmoney-ui/src/app/components/book-entry/book-entry.module.ts
@@ -0,0 +1,38 @@
+import { TableModule } from 'primeng/table';
+import { DropdownModule } from 'primeng/dropdown';
+import { CurrencyMaskModule } from 'ng2-currency-mask';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { CalendarModule } from 'primeng/calendar';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { TooltipModule } from 'primeng/tooltip';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { BookEntryGridComponent } from './book-entry-grid/book-entry-grid.component';
+import { BookEntryListComponent } from './book-entry-list/book-entry-list.component';
+import { BookEntryCreateComponent } from './book-entry-create/book-entry-create.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    BookEntryCreateComponent,
+    BookEntryListComponent,
+    BookEntryGridComponent,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    TooltipModule,
+    InputTextareaModule,
+    CalendarModule,
+    SelectButtonModule,
+    DropdownModule,
+    CurrencyMaskModule,
+  ],
+  exports: [BookEntryCreateComponent, BookEntryListComponent],
+})
+export class BookEntryModule {}
---
+++ abmoney-angular-ui/abmoney-ui-primeng/abmoney-ui/src/app/components/person/person.module.ts
@@ -0,0 +1,38 @@
+import { FormsModule } from '@angular/forms';
+import { InputMaskModule } from 'primeng/inputmask';
+import { DropdownModule } from 'primeng/dropdown';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { CalendarModule } from 'primeng/calendar';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { TooltipModule } from 'primeng/tooltip';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { TabViewModule } from 'primeng/tabview';
+import { PersonGridComponent } from './person-grid/person-grid.component';
+import { PersonCreateComponent } from './person-create/person-create.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PersonListComponent } from './person-list/person-list.component';
+
+
+
+@NgModule({
+  declarations: [PersonCreateComponent, PersonGridComponent, PersonListComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    TabViewModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    TooltipModule,
+    InputTextareaModule,
+    CalendarModule,
+    SelectButtonModule,
+    DropdownModule,
+    InputMaskModule
+  ],
+  exports: [PersonCreateComponent, PersonGridComponent, PersonListComponent]
+})
+export class PersonModule { }
